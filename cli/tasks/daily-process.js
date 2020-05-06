8367f406bf99de98cc00f28f10b16f5c2f43ac8b
michaelrambeau <michael@medmain.com>
2020-05-06 22:01:46 +0900

    Handle `LOCK` env variable to avoid unwanted deploys

--- cli/tasks/daily-process.js
+++ cli/tasks/daily-process.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const { runTasks } = require("../task-runner");
 
 const updateProjects = require("./projects/update-github-data.task");
@@ -6,14 +7,17 @@ const updateHeroes = require("./hall-of-fame/update-github-heroes.task");
 const buildHeroes = require("./hall-of-fame/build-heroes.task");
 const notify = require("./notify.task");
 
-const isGitHubDeployment = process.env.VERCEL_GITHUB_DEPLOYMENT;
-const commitSHA = process.env.VERCEL_GITHUB_COMMIT_SHA;
+const isDeploymentLocked = process.env.LOCK === "1";
 
-console.log({ isGitHubDeployment, commitSHA });
-
-if (isGitHubDeployment) {
-  console.info(`No build process to trigger after a branch is pushed`);
+if (isDeploymentLocked) {
+  console.info(
+    `Build process is locked!
+     Unlock by updating "LOCK_DEPLOY" from the settings page
+    `
+  );
   process.exit(0);
+} else {
+  console.info(`Deployments are unlocked, starting the building process...`);
 }
 
 // Compose the tasks that need to be run every day
