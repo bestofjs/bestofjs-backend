4f50a9e71c277f2c113dcdd7f5a6b353b0fdaafc
michaelrambeau <michael@medmain.com>
2021-06-13 19:16:24 +0900

    Send daily rankings notification to Slack and Discord

--- cli/tasks/notify.task.js
+++ cli/tasks/notify.task.js
@@ -9,38 +9,85 @@ module.exports = createTask("notify-hot-projects", async (context, options) => {
   const { logger } = context;
   const { channel, dryRun } = options;
 
+  const slackURL = process.env.SLACK_DAILY_WEBHOOK;
+  if (!slackURL) throw new Error('No "SLACK_WEBHOOK" env. variable defined');
+  const discordURL = process.env.DISCORD_DAILY_WEBHOOK;
+  if (!discordURL)
+    throw new Error('No "DISCORD_WEBHOOK" env. variable defined');
+
+  const projects = await fetchHottestProjects();
+
   logger.debug("Send the daily notification...", options);
-  const data = await readFile();
-  const { projects } = data;
-  logger.debug(`${projects.length} projects to scan`);
-  await notify({ projects, channel, dryRun });
-  logger.info("Notification sent to Slack");
+
+  if (await notifySlack({ projects, url: slackURL, channel, dryRun })) {
+    logger.info("Notification sent to Slack");
+  }
+
+  if (await notifyDiscord({ projects, url: discordURL, dryRun })) {
+    logger.info("Notification sent to Discord");
+  }
 });
 
-function readFile() {
+async function fetchHottestProjects() {
+  const data = await readRankingsFile();
+  const { projects } = data;
+
+  const score = project =>
+    project.trends.daily > 0 ? project.trends.daily : 0;
+
+  const topProjects = projects
+    .sort((a, b) => (score(a) > score(b) ? -1 : 1))
+    .slice(0, 5);
+  return topProjects;
+}
+
+function readRankingsFile() {
   const filePath = path.join(process.cwd(), "build", "projects.json");
   return fs.readJson(filePath);
 }
 
-function notify({ projects, channel, dryRun }) {
-  const url = process.env.SLACK_DAILY_WEBHOOK;
-  if (!url) throw new Error('No "SLACK_WEBHOOK" env. variable defined');
-  const score = project =>
-    project.trends.daily > 0 ? project.trends.daily : 0;
-  const topProjects = projects
-    .sort((a, b) => (score(a) > score(b) ? -1 : 1))
-    .slice(0, 5);
-  const options = { url, projects: topProjects, channel, dryRun };
-  // In local, override the default Slack channel to avoid sending messages to the real channel
-  if (!channel) channel = process.env.SLACK_CHANNEL_TEST;
-  if (channel) options.channel = channel;
-  return notifySuccess(options);
+async function notifySlack({ projects, url, channel, dryRun }) {
+  const text = "TOP 5 Hottest Projects Today";
+
+  const attachments = projects.map((project, i) => {
+    const stars = project.trends.daily;
+    const text = `Number ${i + 1} +${stars} stars since today:`;
+    return projectToSlackAttachment(project, text);
+  });
+
+  if (dryRun) {
+    console.info("[DRY RUN] No message sent to Slack", { text, attachments }); //eslint-disable-line no-console
+    return;
+  }
+
+  await sendMessageToSlack(text, { url, channel, attachments });
+  return true;
+}
+
+async function notifyDiscord({ projects, url, dryRun }) {
+  const text = "TOP 5 Hottest Projects Today";
+  const colors = ["9c0042", "d63c4a", "f76d42", "ffae63", "ffe38c"]; // hex colors without the `#`
+
+  const embeds = projects.map((project, index) => {
+    const stars = project.trends.daily;
+    const text = `+${stars} stars since today [number ${index + 1}]`;
+    const color = colors[index];
+    return projectToDiscordEmbed(project, text, color);
+  });
+
+  if (dryRun) {
+    console.info("[DRY RUN] No message sent to Discord", { text, embeds }); //eslint-disable-line no-console
+    return;
+  }
+
+  await sendMessageToDiscord(text, { url, embeds });
+  return true;
 }
 
 // Convert a `project` object (from bestofjs API)
 // into an "attachment" included in the Slack message
 // See: https://api.slack.com/docs/message-attachments
-function projectToAttachment(project, pretext) {
+function projectToSlackAttachment(project, pretext) {
   const url = project.url || `https://github.com/${project.full_name}`;
   const owner = project.full_name.split("/")[0];
   const author_name = owner;
@@ -59,8 +106,26 @@ function projectToAttachment(project, pretext) {
   return attachment;
 }
 
+function projectToDiscordEmbed(project, text, color) {
+  const url = project.url || `https://github.com/${project.full_name}`;
+  const thumbnailSize = 50;
+  return {
+    type: "article",
+    title: project.name,
+    url,
+    description: project.description,
+    thumbnail: {
+      url: `https://avatars.githubusercontent.com/u/${project.owner_id}?v=3&s=${thumbnailSize}`,
+      width: thumbnailSize,
+      height: thumbnailSize
+    },
+    color: parseInt(color, 16), // has to be a decimal number
+    footer: { text } // a header would be better to introduce the project
+  };
+}
+
 // Send a message to a Slack channel
-async function sendSlackMessage(text, { url, channel, attachments }) {
+async function sendMessageToSlack(text, { url, channel, attachments }) {
   const body = {
     text,
     mrkdwn: true,
@@ -84,15 +149,22 @@ async function sendSlackMessage(text, { url, channel, attachments }) {
   }
 }
 
-function notifySuccess({ projects, url, channel, dryRun }) {
-  const attachments = projects.map((project, i) => {
-    const stars = project.trends.daily;
-    const text = `Number ${i + 1} +${stars} stars since today:`;
-    return projectToAttachment(project, text);
-  });
-  const text = "TOP 5 Hottest Projects Today";
+async function sendMessageToDiscord(text, { url, embeds }) {
+  const body = {
+    content: text,
+    embeds
+  };
+  try {
+    debug(`Sending webhook to ${url}`, body);
+    const { body: bodyResponse, statusCode } = await got(url, {
+      method: "POST",
+      body: JSON.stringify(body),
+      headers: { "content-type": "application/json" }
+    });
 
-  return dryRun
-    ? console.info("[DRY RUN]", text, attachments) //eslint-disable-line no-console
-    : sendSlackMessage(text, { url, channel, attachments });
+    debug("Response", bodyResponse || "(No content)", statusCode);
+    return true;
+  } catch (error) {
+    throw new Error(`Invalid response from Discord ${error.message}`);
+  }
 }
