fc3ead266892722bd0eb034e15b27a01a435fdde
michaelrambeau <mikeair@gmail.com>
2022-11-24 21:59:37 +0900

    Fix NPM API request after `got` upgrade, attempt 2!

--- core/npm/npm-api-client.js
+++ core/npm/npm-api-client.js
@@ -43,10 +43,8 @@ function createNpmClient({ timeout = 1000 } = {}) {
 
     async fetchMonthlyDownloadCount(packageName) {
       const url = `https://api.npmjs.org/downloads/point/last-month/${packageName}`;
-      console.log(url);
-      const { body } = await got(url);
-      console.log(body);
-      return body.downloads;
+      const  data = await got(url).json();
+      return data.downloads;
     }
   };
 }
